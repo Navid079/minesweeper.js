405fa75b3473a6a5c1f7a93a6e18545c389b933c
Navid079 <navid.naseri.079@gmail.com>
2023-02-18 16:35:55 +0330

    mines added

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const config = {
   border: '#a1a1a1',
 };
 
+function randint(from, to) {
+  return Math.floor(Math.random() * (to - from)) + from;
+}
+
+function range(start, end, step) {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+}
+
 class Tile {
   constructor(x, y, isMine) {
     this.x = x;
@@ -46,9 +56,39 @@ class Tile {
   }
 }
 
+class Game {
+  constructor(w, h, nMines) {
+    this.board = [];
+
+    const mines = this.generateMines(w, h, nMines);
+
+    for (let i = 0; i < h; i++) {
+      const row = [];
+      for (let j = 0; j < w; j++) {
+        row.push(new Tile(j, i));
+      }
+    }
+  }
+
+  generateMines(w, h, nMines) {
+    const count = w * h;
+    const values = range(0, count, 1);
+    const result = [];
+    for (let i = nMines - 1; i >= 0; i--) {
+      const index = randint(0, i);
+      const [value] = values.splice(index, 1);
+      result.push(value);
+    }
+
+    return result;
+  }
+}
+
 function start() {
   const canvas = document.getElementById('navid079:minesweeper');
   if (!canvas) return;
+
+  new Game(10, 10, 10);
 }
 
 onload = start;
